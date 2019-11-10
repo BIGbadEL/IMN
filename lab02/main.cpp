867186ff8da1106de50bce12327efde4e98d96e9
BIGbadEL <greg090912@gmail.com>
2019-11-10 15:22:34 +0100

    finished task 2

--- lab02/main.cpp
+++ lab02/main.cpp
@@ -1,7 +1,7 @@
 #include <cmath>
+#include <functional>
 #include <iostream>
 #include <vector>
-#include <functional>
 
 constexpr double Beta = 0.001;
 constexpr int N = 500;
@@ -11,8 +11,8 @@ constexpr double dt = 0.1;
 constexpr double u0 = 1;
 constexpr double Tol = 1e-6;
 constexpr double alfa = Beta * N - my_gamma;
-constexpr double c1 =  0.5 - 1.732050808 / 6;
-constexpr double c2 =  0.5 + 1.732050808 / 6;
+constexpr double c1 = 0.5 - 1.732050808 / 6;
+constexpr double c2 = 0.5 + 1.732050808 / 6;
 constexpr double a11 = 0.25;
 constexpr double a12 = 0.25 - 1.732050808 / 6;
 constexpr double a21 = 0.25 + 1.732050808 / 6;
@@ -32,7 +32,8 @@ void vec_to_file(const std::vector<Point> &vec, const char *name) {
   fclose(file);
 }
 
-void solution(std::vector<Point> &u, std::vector<Point> &z, std::function<double(double , double)> fun) {
+void solution(std::vector<Point> &u, std::vector<Point> &z,
+              std::function<double(double, double)> fun) {
   u.push_back(Point{0.0, u0});
   for (double t = dt; t <= tmax; t += dt) {
     double u_next = u.back().y;
@@ -44,6 +45,46 @@ void solution(std::vector<Point> &u, std::vector<Point> &z, std::function<double
       count++;
     } while (std::abs(u_next - temp) > Tol && count <= 20);
     u.push_back(Point{t, u_next});
+  }
+  for (auto el : u) {
+    z.push_back(Point{el.x, N - el.y});
+  }
+}
+
+void RK2(std::vector<Point> &u, std::vector<Point> &z) {
+  u.push_back(Point{0.0, u0});
+  auto F1 = [](double u1, double u2, double un) {
+    return u1 - un -
+           dt * (a11 * (alfa * u1 - Beta * u1 * u1) +
+                 a12 * (alfa * u2 - Beta * u2 * u2));
+  };
+  auto F2 = [&F1](double u2, double u1, double un) {
+    return F1(u2, u1, un);
+  };
+  auto f = [](double t, double u) {
+    return (Beta * N - my_gamma) * u - Beta * u * u;
+  };
+
+  for (double t = dt; t <= tmax; t += dt) {
+    double u_next = u.back().y;
+    double U1 = u_next;
+    double U2 = u_next;
+    double m11 = 1 - dt * a11 * (alfa - 2 * Beta * U1);
+    double m12 = - dt * a12 * (alfa - 2 * Beta * U2);
+    double m21 = - dt * a21 * (alfa - 2 * Beta * U1);
+    double m22 = 1 - dt * a22 * (alfa - 2 * Beta * U2);
+
+    int count = 0;
+    double dU1;
+    double dU2;
+    do {
+      dU1 = (F2(U1, U2, u_next) * m12 - F1(U1, U2, u_next) * m22) / (m11 * m22 - m12 * m21);
+      dU2 = (F1(U1, U2, u_next) - F2(U1, U2, u_next)) / (m11 * m22 - m12 * m21);
+      U1 += dU1;
+      U2 += dU2;
+      count++;
+    } while (std::abs(dU1) > Tol && std::abs(dU2) > Tol && count <= 20);
+    u.push_back(Point{t, u_next + dt * (b * f(t + c1 * dt, U1) + b * f(t + c2 * dt, U2))});
   }
   for (auto el : u) {
     z.push_back(Point{el.x, N - el.y});
@@ -68,10 +109,15 @@ int main() {
                     dt / 2. *
                         ((alfa * un - Beta * un * un) +
                          (alfa * u_mid - Beta * u_mid * u_mid))) /
-                       (1 - dt / 2.0 * ( alfa - 2 * Beta * u_mid));
+                       (1 - dt / 2.0 * (alfa - 2 * Beta * u_mid));
   };
-  solution(u , z, newton_lambda);
+  solution(u, z, newton_lambda);
   vec_to_file(u, "u(t)_new.dat");
   vec_to_file(z, "z(t)_new.dat");
+  u.clear();
+  z.clear();
+  RK2(u, z);
+  vec_to_file(u, "u(t)_RK2.dat");
+  vec_to_file(z, "z(t)_RK2.dat");
   return 0;
 }
