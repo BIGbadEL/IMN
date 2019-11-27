196de2ecfb0b59b59adaec06e33da2efdfbb5101
BIGbadEL <greg090912@gmail.com>
2019-11-27 23:34:04 +0100

    lab04-xd

--- lab04/4.plot
+++ lab04/4.plot
@@ -1,5 +1,9 @@
 set term png 
 
+set out "error_z1_0_6.png"
+plot "error_1_0.600000.dat" u 1:2:3 with image
+
+
 set logscale x
 
 set out "z1.png"
@@ -13,4 +17,5 @@ set out "z2.png"
 set xl "nr iteracji"
 set yl "S"
 plot "2_1.000000.dat" w l , "2_1.400000.dat" w l , "2_1.800000.dat" w l , "2_1.900000.dat" w l
+
 
--- lab04/main.cpp
+++ lab04/main.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-#include <cmath>
 #include <array>
+#include <cmath>
 #include <functional>
+#include <iostream>
 
 constexpr double epsilon = 1.0;
 constexpr double delta = 0.1;
@@ -17,24 +17,24 @@ using my_array = std::array<std::array<double, b>, a>;
 
 my_array<nx + 1, ny + 1> fill_array(double v1, double val, double v2) {
     my_array<nx + 1, ny + 1> result;
-    for(auto& array : result) {
-        for(auto& element : array){
+    for (auto& array : result) {
+        for (auto& element : array) {
             element = val;
         }
     }
-    for(int i = 0; i < ny + 1; i++) {
+    for (int i = 0; i < nx + 1; i++) {
         result[i][0] = v1;
         result[i][ny] = v2;
     }
     return result;
 }
 
-constexpr double p1_val(double x, double y){
-    return std::exp(- std::pow(x - 0.35 * xmax, 2) / (dx * dx) - std::pow(y - 0.5 * ymax, 2) / (dy * dy));
+constexpr double p1_val(double x, double y) {
+    return std::exp(-std::pow(x - 0.35 * xmax, 2) / (dx * dx) - std::pow(y - 0.5 * ymax, 2) / (dy * dy));
 }
 
-constexpr double p2_val(double x, double y){
-    return - std::exp(- std::pow(x - 0.65 * xmax, 2) / (dx * dx) - std::pow(y - 0.5 * ymax, 2) / (dy * dy));
+constexpr double p2_val(double x, double y) {
+    return -std::exp(-std::pow(x - 0.65 * xmax, 2) / (dx * dx) - std::pow(y - 0.5 * ymax, 2) / (dy * dy));
 }
 
 constexpr double p_val(double x, double y) {
@@ -43,54 +43,54 @@ constexpr double p_val(double x, double y) {
 
 my_array<nx, ny> fillP() {
     my_array<nx, ny> result;
-    for(std::size_t i = 0; i < nx; i++){
-        for(std::size_t j = 0; j < ny; j++) {
+    for (std::size_t i = 0; i < nx; i++) {
+        for (std::size_t j = 0; j < ny; j++) {
             result[i][j] = p_val(i * delta, j * delta);
         }
     }
     return result;
 }
 
-constexpr double nextV(const my_array<nx, ny>& p, const my_array<nx + 1, ny + 1>& v, std::size_t i, std::size_t j) {
-    return 0.25 * (v[i+1][j] + v[i-1][j] + v[i][j+1] + v[i][j-1] + delta * delta * p[i][j] / epsilon );
+constexpr double nextV(const my_array<nx, ny>& p,
+    const my_array<nx + 1, ny + 1>& v, std::size_t i,
+    std::size_t j) {
+    return 0.25 * (v[i + 1][j] + v[i - 1][j] + v[i][j + 1] + v[i][j - 1] + delta * delta * p[i][j] / epsilon);
 }
 
-my_array<nx + 1, ny + 1> fill_new_V(my_array<nx + 1, ny + 1>& act_v, my_array<nx + 1, ny + 1>& old_v, const my_array<nx, ny>& p, double wg) {
+my_array<nx + 1, ny + 1> fill_new_V(my_array<nx + 1, ny + 1>& act_v,
+    my_array<nx + 1, ny + 1>& old_v,
+    const my_array<nx, ny>& p, double wg) {
     my_array<nx + 1, ny + 1> result = act_v;
-    for(std::size_t i = 1; i < nx; i++) {
-        for(std::size_t j = 1; j < ny; j++) {
+    for (std::size_t i = 1; i < nx; i++) {
+        for (std::size_t j = 1; j < ny; j++) {
             result[i][j] = nextV(p, old_v, i, j);
         }
     }
-    for(std::size_t j = 0; j < ny + 1; j++) {
+    for (std::size_t j = 0; j < ny + 1; j++) {
         result[0][j] = result[1][j];
         result[nx][j] = result[nx - 1][j];
     }
-    for(std::size_t i = 0; i < nx + 1; i++) {
-        for(std::size_t j = 0; j < ny + 1; j++) {
+    for (std::size_t i = 0; i < nx + 1; i++) {
+        for (std::size_t j = 0; j < ny + 1; j++) {
             old_v[i][j] = (1 - wg) * old_v[i][j] + wg * result[i][j];
         }
     }
     return result;
 }
 
-constexpr double calculate_S(const my_array<nx, ny>& p, const my_array<nx + 1, ny + 1>& v) {
+constexpr double calculate_S(const my_array<nx, ny>& p,
+    const my_array<nx + 1, ny + 1>& v_n) {
     double result = 0.0;
     for (std::size_t i = 0; i < nx; i++) {
         for (std::size_t j = 0; j < ny; j++) {
-            double temp = 0.0;
-            temp += 0.5 * std::pow((v[i+1][j] - v[i][j]) / delta, 2);
-            temp += 0.5 * std::pow((v[i][j+1] - v[i][j]) / delta, 2);
-            temp -= p[i][j] * v[i][j];
-            temp *= delta * delta;
-            result += temp;
+            result += (delta * delta) * (0.5 * std::pow((v_n[i + 1][j] - v_n[i][j]) / delta, 2) + 0.5 * std::pow((v_n[i][j + 1] - v_n[i][j]) / delta, 2) - p[i][j] * v_n[i][j]);
         }
     }
     return result;
 }
 
 void global_relaxation(const my_array<nx, ny>& p, double wg, double TOL) {
-    my_array<nx + 1, ny + 1> next_v;
+    my_array<nx + 1, ny + 1> next_v = fill_array(10.0, 0.0, 0.0);
     my_array<nx + 1, ny + 1> start_v = fill_array(10.0, 0.0, 0.0);
     double Sit_prev = 0.0;
     double Sit = calculate_S(p, start_v);
@@ -98,15 +98,24 @@ void global_relaxation(const my_array<nx, ny>& p, double wg, double TOL) {
     int i = 0;
     do {
         Sit_prev = Sit;
-        next_v = fill_new_V(next_v ,start_v, p, wg);
+        next_v = fill_new_V(next_v, start_v, p, wg);
         Sit = calculate_S(p, next_v);
-        fprintf(file, "%d %f\n", ++i, Sit_prev);
-    } while(std::abs((Sit - Sit_prev) / Sit_prev) > TOL);
+        fprintf(file, "%d %f\n", i++, Sit);
+    } while (std::abs((Sit - Sit_prev) / Sit_prev) > TOL);
+    fclose(file);
+    file = fopen(("error_1_" + std::to_string(wg) + ".dat").c_str(), "w");
+    for (int i = 1; i < nx; i++) {
+        for (int j = 1; j < ny; j++) {
+            double e = ((next_v[i + 1][j] - 2 * next_v[i][j] + next_v[i - 1][j]) / (delta * delta) + (next_v[i][j + 1] - 2 * next_v[i][j] + next_v[i][j - 1]) / (delta * delta)) + p[i][j] / epsilon;
+            fprintf(file, "%f %f %f\n", i * delta, j * delta, e);
+        }
+    }
     fclose(file);
 }
 
-double nextV_local(double wl, const my_array<nx, ny>& p, const my_array<nx+1, ny+1>& v, int i, int j) {
-    return (1 - wl) * v[i][j] + wl / 4.0 * (v[i+1][j] + v[i-1][j] + v[i][j+1] + v[i][j-1] + delta * delta / epsilon * p[i][j]);
+double nextV_local(double wl, const my_array<nx, ny>& p,
+    const my_array<nx + 1, ny + 1>& v, int i, int j) {
+    return (1 - wl) * v[i][j] + wl / 4.0 * (v[i + 1][j] + v[i - 1][j] + v[i][j + 1] + v[i][j - 1] + delta * delta / epsilon * p[i][j]);
 }
 
 void local_relaxation(const my_array<nx, ny>& p, double wl, double TOL) {
@@ -117,18 +126,26 @@ void local_relaxation(const my_array<nx, ny>& p, double wl, double TOL) {
     int i = 0;
     do {
         Sit_prev = Sit;
-        for(int i = 1; i < nx; i++) {
-            for(int j = 1; j < ny; j++) {
+        for (int i = 1; i < nx; i++) {
+            for (int j = 1; j < ny; j++) {
                 v[i][j] = nextV_local(wl, p, v, i, j);
             }
         }
-        for(int j = 0; j < ny; j++){
+        for (int j = 1; j < ny; j++) {
             v[0][j] = v[1][j];
             v[nx][j] = v[nx - 1][j];
         }
         Sit = calculate_S(p, v);
         fprintf(file, "%d %f\n", i++, Sit);
-    } while(std::abs((Sit - Sit_prev) / Sit_prev) > TOL);
+    } while (std::abs((Sit - Sit_prev) / Sit_prev) > TOL);
+    fclose(file);
+    file = fopen(("error_2_" + std::to_string(wl) + ".dat").c_str(), "w");
+    for (int i = 1; i < nx; i++) {
+        for (int j = 1; j < ny; j++) {
+            double e = ((v[i + 1][j] - 2 * v[i][j] + v[i - 1][j]) / (delta * delta) + (v[i][j + 1] - 2 * v[i][j] + v[i][j - 1]) / (delta * delta)) + p[i][j] / epsilon;
+            fprintf(file, "%f %f %f\n", i * delta, j * delta, e);
+        }
+    }
     fclose(file);
 }
 
